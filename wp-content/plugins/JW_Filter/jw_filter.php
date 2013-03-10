deeaeb5f30172f055fd9865d55c188fb4089beaf
phx-hp1000 <306761352@qq.com>
2013-03-10 20:10:06 +0800

    a

--- wp-content/plugins/JW_Filter/jw_filter.php
+++ wp-content/plugins/JW_Filter/jw_filter.php
@@ -14,5 +14,66 @@ add_filter( 'the_content', function( $content ) {
 		return $content . ' ' . time();
 	} );
 
+// 在每个文章后显示同属于该category下的其他文章
+add_filter( 'the_content', function( $content ){
+	$id = get_the_id();
+
+	if (!is_singular( 'post' )) {
+		return $content;
+	}
+
+	$terms = get_the_terms( $id, 'category' );
+	$cats = array();
+
+	foreach ($terms as $term) {
+		$cats[] = $term->cat_ID;
+	}
+
+	$loop = new WP_Query(
+		array(
+				'posts_per_page' => 3,
+				'category__in' => $cats,
+				'orderby' => 'rand',
+				'post__not_in' => array($id)
+		)
+	);
+
+	if ($loop->have_posts()) {
+		$content .= '
+			<h2>You also might like ...</h2>
+			<ul class="related-catogory-posts">';
+
+		while ($loop->have_posts()) {
+			$loop->the_post();
+
+			$content .= '
+				<li>
+					<a href="'.get_permalink().'">'.get_the_title().'</a>
+				</li>
+			';
+		}
+
+		$content .= '</ul>';
+		wp_reset_query();
+	}
+
+	return $content;
+
+});
+
+add_action( 'wp_footer', function(){
+	echo '这是在footer位置添加的文本';
+} );
+
+add_action( 'comment_post', function(){
+	$email = get_bloginfo( 'admin_email' );
+	wp_mail(
+		$email,
+		'New comment posted',
+		'This is the comment'
+		);
+});
+
+
 
 
