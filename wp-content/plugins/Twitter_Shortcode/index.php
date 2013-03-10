99c77b14a2d2f7ac0aceebf53c50a802edff435a
phx-hp1000 <306761352@qq.com>
2013-03-10 21:08:46 +0800

    a

--- wp-content/plugins/Twitter_Shortcode/index.php
+++ wp-content/plugins/Twitter_Shortcode/index.php
@@ -8,10 +8,33 @@ Author URI: http://www.ttt.com
 Version: 1.0
 */
 
-// [twitter username="test"]
-add_shortcode( 'twitter', function($attributes){
+// [twitter1 username="test"]
+add_shortcode( 'twitter1', function($attributes){
 	if (!isset($attributes['username'])) $attributes['username'] = 'phoenixg';
 	return '<a href="http://twitter.com/'.$attributes['username'].'">
 		Follow me on Twitter!
 	</a>';
+} );
+
+
+// [twitter2 username="test"] Follow me! [/twitter2]
+/*
+add_shortcode( 'twitter2', function($attributes, $content){
+	if (!isset($attributes['username'])) $attributes['username'] = 'phoenixg';
+	return '<a href="http://twitter.com/'.$attributes['username'].'">'.
+		$content.'</a>';
+} );
+*/
+add_shortcode( 'twitter2', function($attributes, $content){
+	// 设置属性的默认值
+	$attributes =  shortcode_atts( 
+						array(
+							'username' => 'phoenixg',
+							'content' => !empty($content) ? $content : 'Follow me!'
+						),$attributes
+					);
+
+	extract($attributes);
+
+	return "<a href='http://twitter.com/{$username}'>{$content}</a>";
 } );
